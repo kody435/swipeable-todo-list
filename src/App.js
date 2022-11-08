507dbb502fb4cc86c2080fd457c92fe463db866b
Cody <50862704+kody435@users.noreply.github.com>
2022-11-08 21:15:41 +0530

    fcuk u

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  background-image: linear-gradient(to top, #ffa6a1, #ff584f);
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Wrapper = styled.div`
+  max-width:600px;
+  min-width: 455px;
+  min-height: 50vh;
+  padding: 20px;
+  background-color: #fff;
+  box-shadow: 5px 2px 44px 1px rgba(0,0,0,0.75) ;
+`;
+
+const Title = styled.h1`
+  font-weight: 600;
+  text-align: center;
+`;
 
 const App = () => {
   return (
-    <h1>Todo List</h1>
+    <Container>
+      <Wrapper>
+        <Title>Todo List</Title>
+      </Wrapper>
+    </Container>
   )
 }
 
