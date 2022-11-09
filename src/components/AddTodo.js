e09a3b0f9f146868515e3afb1a0171b508fb8a57
Cody <50862704+kody435@users.noreply.github.com>
2022-11-09 07:55:25 +0530

    ya ya whatever

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React , { useState } from 'react'
 import styled from 'styled-components';
 
-const styledInput = styled.input`
+const StyledInput = styled.input`
     width: 80%;
     padding:12px 20px;
     margin: 8px 0;
     display: inline-block;
     border: 2px solid #ccc;
-    border-radius: 4px;
+    border-radius: 25px;
 `;
 
 const AddTodo = () => {
+
+    const [title,setTitle] = useState('');
+
     return (
-        <form style={{display:"flex", alignItems:"center"}}>
-            <styledInput type="text" name="title" placeholder="Add Todo..." />
-        </form>
-    )
+      <form style={{ display: "flex", alignItems: "center" }}>
+        <StyledInput
+            type="text"
+            name="title"
+            placeholder="Add Todo..."
+            onChange={(e) => setTitle(e.target.value)}
+            required
+        />
+      </form>
+    );
 }
 
 export default AddTodo
