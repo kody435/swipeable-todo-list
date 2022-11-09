4f7eeaeaecbbd25b7496df76d37ed80c7ecee7a0
Cody <50862704+kody435@users.noreply.github.com>
2022-11-09 08:08:29 +0530

    made some visible changes

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -4,10 +4,24 @@ import styled from 'styled-components';
 const StyledInput = styled.input`
     width: 80%;
     padding:12px 20px;
-    margin: 8px 0;
+    margin: 8px;
     display: inline-block;
     border: 2px solid #ccc;
     border-radius: 25px;
+`;
+
+const StyledButton = styled.button`
+    background-image: linear-gradient(to right, #ffa6a1, #ff584f);
+    width: 20%;
+    cursor: pointer;
+    border-radius: 20px;
+    height: 20%;
+    border: none;
+    margin-left: 10px;
+    color: #fff;
+    padding: 12px;
+    font-size: 16px;
+    font-weight:bold;
 `;
 
 const AddTodo = () => {
@@ -22,7 +36,8 @@ const AddTodo = () => {
             placeholder="Add Todo..."
             onChange={(e) => setTitle(e.target.value)}
             required
-        />
+            />
+            <StyledButton type="submit">Add</StyledButton>
       </form>
     );
 }
