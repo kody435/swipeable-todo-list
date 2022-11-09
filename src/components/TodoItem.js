83b547bd8ed31605b40e394bf40e3c2e7db81f51
Cody <50862704+kody435@users.noreply.github.com>
2022-11-09 11:04:11 +0530

    hello

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,8 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 
+const TitleContainer = styled.div`
+    display: flex;
+    flex: 1;
+    margin-top: 10px;
+    background-color: #f4f4f4;
+    padding:10px 20px;
+    border-radius: 50px;
+    font-weight: bold;
+    text-decoration: none;
+    cursor: pointer;
+`;
+
 export default function TodoItem({ todo }) {
     return (
-        <div>{todo?.title}</div>
+        <TitleContainer>{todo?.title}</TitleContainer>
     )
 }
